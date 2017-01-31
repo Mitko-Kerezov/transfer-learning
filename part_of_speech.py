45bff2c59c27521dab8555538cb2254bb97ebeba
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2017-02-01 00:51:05 +0200

    Fix part_of_speech code and charts

--- part_of_speech.py
+++ part_of_speech.py
@@ -1,4 +1,4 @@
-"""This module does presents graphics about what part of speech each of the tags represent."""
+"""This module presents graphics about what part of speech each of the tags represent."""
 import csv
 from os.path import join
 from collections import defaultdict
@@ -7,18 +7,18 @@ from nltk.stem import WordNetLemmatizer
 import matplotlib.pyplot as plt
 
 
-def print_tags(csv_path, csv_name):
+def save_part_of_speech_charts(csv_path, csv_name):
     print("Working on %s" % csv_path)
     wnl = WordNetLemmatizer()
     reader = csv.DictReader(open(csv_path, encoding="utf8"))
     tags_table = defaultdict(int)
     tags_num = 0
-    for line in reader:
-        for pair in pos_tag(list(map(wnl.lemmatize, line["tags"].split(" ")))):
+    for post in reader:
+        for pair in pos_tag(list(map(wnl.lemmatize, post["tags"].split(" ")))):
             tags_table[pair[1]] += 1
             tags_num += 1
     for key in tags_table:
-        tags_table[key] /= tags_num
+        tags_table[key] = tags_table[key] / tags_num * 100
     plt.figure(figsize=(20, 10))
     plt.bar(range(len(tags_table)), tags_table.values(), align='center')
     plt.xticks(range(len(tags_table)), tags_table.keys())
@@ -28,4 +28,4 @@ def print_tags(csv_path, csv_name):
     plt.savefig(join("charts", "part_of_speech", csv_name + ".png"))
 
 for csv_name in ['biology', 'cooking', 'crypto', 'diy', 'robotics', 'travel']:
-    print_tags(join("data", csv_name + ".csv"), csv_name)
+    save_part_of_speech_charts(join("data", csv_name + ".csv"), csv_name)
